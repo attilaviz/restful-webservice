0f5f6bc903d05a4af3360472027f0410ac4413bf
Attila Vizhanyo <attila.vizhanyo@netcetera.com>
2020-12-29 12:07:33 +0100

    Added post mapping.

--- src/main/java/com/baeldung/restfulwebservice/controller/FooController.java
+++ src/main/java/com/baeldung/restfulwebservice/controller/FooController.java
@@ -3,9 +3,8 @@ package com.baeldung.restfulwebservice.controller;
 import com.baeldung.restfulwebservice.model.Foo;
 import com.baeldung.restfulwebservice.service.IFooService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +18,11 @@ public class FooController {
     @GetMapping
     public List<Foo> findAll() {
         return service.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Foo create(@RequestBody Foo foo) {
+        return service.save(foo);
     }
 }
